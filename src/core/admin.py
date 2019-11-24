f9c7d1ed71901e2499c7d56c103058fa6390e01a
MoinulHassinaNabil <nnaabill@gmail.com>
2019-11-24 00:30:16 -0700

    registered a model named BillingAddress

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
+admin.site.register(BillingAddress)
